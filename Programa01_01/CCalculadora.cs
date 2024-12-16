7a5820db9338a3dee3ca4f5bf4c7ba9c8c60e2c5
alexanderlopez1000 <alexanderlopez.1000@gmail.com>
2024-12-15 20:51:11 -0500

    Programa01_01

---
+++ Programa01_01/CCalculadora.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programa01_01
+{
+    internal class CCalculadora
+    {
+        //Declaramos los atributos
+        public double a;
+        public double b;
+        private double r;
+
+        //Declaramos los metodos 
+        public void Suma()
+        {
+            r = a + b;
+        }
+        public void Muestra()
+        {
+            Console.WriteLine("a={0},b={1},c={2}",a,b,r);
+                }
+
+    }
+}
--- Programa01_01/Program.cs
+++ Programa01_01/Program.cs
@@ -10,6 +10,15 @@ namespace Programa01_01
     {
         static void Main(string[] args)
         {
+            //Creamos una instancia
+            CCalculadora calcu1 = new CCalculadora();
+            //accedemos a los atributos para mostrar info 
+            calcu1.a = 5;
+            calcu1.b = 3;
+            calcu1.Muestra();
+            calcu1.Suma();
+            calcu1.Muestra();
+
         }
     }
 }
